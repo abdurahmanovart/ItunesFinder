392c7d5bf1c63175aa033e6e908cf470eba6f431
Abdurakhmanov Artur <abdurahmanovart1994@gmail.com>
2017-08-13 13:13:50 +0300

    initial commit

--- app/src/main/java/com/github/abdurahmanovart/itunesfinder/MainActivity.java
+++ app/src/main/java/com/github/abdurahmanovart/itunesfinder/MainActivity.java
@@ -1,13 +1,45 @@
 package com.github.abdurahmanovart.itunesfinder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+        createSearchView();
+    }
+
+    private void createSearchView() {
+        SearchView SearchView = (SearchView) findViewById(R.id.search);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        SearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // TODO: 13.08.17 use query for apt
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 }
