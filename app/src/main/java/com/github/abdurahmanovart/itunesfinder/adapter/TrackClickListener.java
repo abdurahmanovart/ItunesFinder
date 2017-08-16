257f3c1fb14989e2f607090ee56b2f8c908adb0c
Abdurakhmanov Artur <abdurahmanovart1994@gmail.com>
2017-08-16 21:43:24 +0300

    added adapter

---
+++ app/src/main/java/com/github/abdurahmanovart/itunesfinder/adapter/TrackClickListener.java
@@ -0,0 +1,9 @@
+package com.github.abdurahmanovart.itunesfinder.adapter;
+
+/**
+ * @author Abdurakhmanov on 15.08.17
+ */
+
+public interface TrackClickListener {
+    void onClick(int position);
+}
